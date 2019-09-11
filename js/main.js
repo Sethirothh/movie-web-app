0169f472103d3ac10b03c9de48c67116e098b272
Adi George <adi_george@outlook.com>
2019-09-11 17:26:00 +0200

    static slideshow

--- js/main.js
+++ js/main.js
@@ -87,3 +87,20 @@ if ($('body').hasClass('mobileOpen')) {
 }
 
 //slideshow
+
+var swiper = new Swiper('.swiper-container', {
+   effect: 'coverflow',
+   grabCursor: true,
+   centeredSlides: true,
+   slidesPerView: 'auto',
+   coverflowEffect: {
+     rotate: 20,
+     stretch: 0,
+     depth: 600,
+     modifier: 1,
+     slideShadows : true,
+   },
+   pagination: {
+     el: '.swiper-pagination',
+   },
+ });
