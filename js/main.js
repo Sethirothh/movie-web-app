a0d1c1058288dcf5bbd2096736206f2c5d044c41
Jesper Gyldenløve <jespergyldenlve@gmail.com>
2019-09-12 12:06:36 +0200

    Working Nav-bar

--- js/main.js
+++ js/main.js
@@ -47,45 +47,37 @@ setDefaultPage();
 function randomMovie(){
   console.log(Math.floor(Math.random() * 10));
 }
+function openModal(){
 
-
-// navigation
-$('nav').hide();
-$('.fa-bars').click(function () {
-    console.log('clicked');
-    $('.mobileNav').slideToggle();
-    $('body').addClass('mobileOpen');
-    $('.fa-times-circle').css('display', 'block');
-    $('.fa-bars').css('display', 'none');
-
-});
-
-
-$('.fa-times-circle').click(function () {
-    $('.fa-times-circle').css('display', 'none');
-    $('.fa-bars').css('display', 'block');
-});
-
-
-$('.fa-times-circle').click(function () {
-    if ($('body').hasClass('mobileOpen')) {
-        console.log('mobile dropdown is closed');
-        $('.mobileNav').slideUp();
-        $('body').removeClass('mobileOpen');
-        $('.fa-times-circle').css('display', 'none');
-        $('.fa-bars').css('display', 'block');
-    }
-});
-
-
-
-if ($('body').hasClass('mobileOpen')) {
-    console.log('x');
-} else {
-    $('.fa-times-circle').css('display', 'none');
-    $('.fa-bars').css('display', 'block');
 }
 
+// This handles the navigation
+function toggleMenu(){
+    let nav = document.querySelector(".hamburgerMenu");
+    let ul = document.querySelector("nav > ul");
+  if (nav.classList.contains("activeMenu")) {
+    nav.classList.remove("activeMenu");
+    ul.style.height = 0;
+  } else {
+    nav.classList.add("activeMenu");
+    ul.classList.add("navigationActive");
+    ul.style.height = ul.childElementCount * 50 + "px";
+  }
+}
+
+function infoModal(){
+    let info = document.querySelector("#information");
+
+    if (info.classList.contains("information")) {
+      info.classList.remove("information");
+
+      info.style.display = "none";
+    }
+    else {
+      info.classList.add("information");
+      info.style.display = "block";
+    }
+}
 //slideshow
 
 var swiper = new Swiper('.swiper-container', {
