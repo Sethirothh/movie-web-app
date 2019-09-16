dc0708bb4856f4a8a3bf2ba355335df77b065754
Adi George <adi_george@outlook.com>
2019-09-16 11:36:40 +0200

    Spoiler changes

--- js/spoilers.js
+++ js/spoilers.js
@@ -13,10 +13,12 @@ console.log(movie);
   let htmlTemplate = "";
   htmlTemplate = `
     <article>
-    <button onclick="chosenMovie(${id})">Go Back</button>
-    <h1>True or false</h1>
-    <h2>${movie.data().statement}</h2>
-      </article>
+      <button onclick="chosenMovie(${id})">Go Back</button>
+      <h1>True or false: ${movie.data().title}</h1>
+      <h2>${movie.data().statement}</h2>
+      <img src="${movie.data().img}">
+
+    </article>
     `;
   document.querySelector("#spoilers").innerHTML = htmlTemplate;
 }
