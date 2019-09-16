c9d34427a390349307fb7e65e4b3d98c9a0fdc3b
Karolina Zapolskyte <43141874+KarolinaZapolskyte@users.noreply.github.com>
2019-09-16 13:46:06 +0200

    Profile page

--- js/user.js
+++ js/user.js
@@ -64,8 +64,68 @@ function logInHide() {
 }
 
 function appendUserData(user) {
-  document.querySelector('#profile').innerHTML += `
-    <h3>${user.displayName}</h3>
-    <p>${user.email}</p>
+  let htmlTemplate = "";
+  htmlTemplate += `
+  <div class="text-info">
+    <h2 class="title-box">Your profile</h2>
+    <div class="description-box">
+    <img src="https://cdn3.iconfinder.com/data/icons/bitcoin-cryptocurrency/512/Icon_2-512.png" id="profile-img">
+    <span></span>
+    <input type='file' id="img" accept="image/*")" onchange="readURL(this);" />
+    <h3 id="name">${user.displayName}</h3>
+    </div>
+    <span class="space"</span>
+    <h2 class="title-box">Your scores</h2>
+    <div class="description-box">
+    <div class="score-board">
+    <p id="total" class="float-left">Total</p>
+    <img src="arrow.png" id="arrow" class="float-right" alt="arrow to the left" onclick="haveScore()">
+    <p id="score" class="float-right">398 Points</p>
+    </div>
+    <div class="score-board-ext slide-in-top">
+      <div class="score-board-row">
+        <p class="movie">Forrest Gump</p>
+        <p class="score">158 Points</p>
+      </div>
+      <div class="score-board-row">
+        <p class="movie">Toy Story 4</p>
+        <p class="score">75 Points</p>
+      </div>
+      <div class="score-board-row">
+        <p class="movie">Lion King</p>
+        <p class="score">165 Points</p>
+      </div>
+    </div>
+    <button id="share-btn"><img src="share.png" id="share" alt="share image">Share</button>
+  </div>
   `;
+  document.querySelector('#profile').innerHTML = htmlTemplate
+}
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    let reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('#profile-img')
+        .attr('src', e.target.result)
+        .width(100)
+        .height(100);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+
+  }
+}
+
+function haveScore() {
+  let arrow = document.querySelector("#arrow");
+  let score = document.querySelector(".score-board-ext");
+  if (score.style.display === "none") {
+    score.style.display = "block";
+    arrow.classList.add('rotate-90-cw');
+  } else {
+    arrow.classList.remove("rotate-90-cw");
+    score.style.display = "none"
+  };
 }
